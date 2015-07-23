e8094c2fd3871ad26ef58424aea24f8462a2ee9e
I311774 <I311774@INLD50873905A.apj.global.corp.sap>
2015-07-23 17:09:23 +0530

    Adding new content.

--- src/Tree/Test.java
+++ src/Tree/Test.java
@@ -1,8 +1,14 @@
 package Tree;
 
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
+		List<String> a = null;
+		for(String s : a) {
+			
+		}
 		System.out.println();
 	}
 }
