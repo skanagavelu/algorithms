35deae0ca747f740a45e62d5701fd67e7fcf63c8
I311774 <I311774@INLD50873905A.apj.global.corp.sap>
2015-09-29 14:17:02 +0530

    commit

---
+++ src/DynamicProgramming/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author I311774
+ *
+ * http://www.geeksforgeeks.org/fundamentals-of-algorithms/#DynamicProgramming
+ * https://en.wikipedia.org/wiki/Optimal_substructure
+ * https://en.wikipedia.org/wiki/Overlapping_subproblems
+ */
+package DynamicProgramming;
--- src/Tree/Test.java
+++ src/Tree/Test.java
@@ -5,10 +5,6 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
-		List<String> a = null;
-		for(String s : a) {
-			
-		}
-		System.out.println();
+
 	}
 }
