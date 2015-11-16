6e298ef364808154677e4b777225286bd5334dbb
I311774 <I311774@INLD50873905A.apj.global.corp.sap>
2015-11-16 18:49:00 +0530

    Updates.

--- src/tree/BTNode.java
+++ src/tree/BTNode.java
@@ -4,8 +4,6 @@ import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class BTNode<T extends Comparable<T>> {
-	public static  BTNodeComparator compator = new BTNodeComparator();
-	
 	public BTNode<T> left;
 	public BTNode<T> right;
 	public T data;
@@ -21,9 +19,9 @@ public class BTNode<T extends Comparable<T>> {
 
 	@Override
 	public String toString() {
-		
 		return this.data.toString();
 	}
+	
 	public static void main(String[] args) {
 		BTNode<String> rt = new BTNode<String>();
 //		rt.data = "5";
@@ -31,20 +29,17 @@ public class BTNode<T extends Comparable<T>> {
 //		rt.left.data = "10";
 //		rt.right = new BTNode<String>();
 //		rt.right.data = "20";
-		
-		String[] dataArray = {"1","2","3","4","#","#","5","#","#","6","#","#","7","8","9","10","#","#","11","#","#","#","#"};
+		String[] dataArray = {"1","2","3","4",null,null,"5",null,null,"6",null,null,"7","8","9","10",null,null,"11",null,null,null,null};
 		rt = buildBTWithPreOrder(dataArray, new AtomicInteger(0));
 		BTDisplay.printTreeNode(rt);
-		
 	}
 	
 
-
-	public static BTNode<String> buildBTWithPreOrder(String[] dataArray, AtomicInteger i){
-		if(i.get() > dataArray.length || dataArray[i.get()].equals("#")) {
+	public static <T extends Comparable<T>> BTNode<T> buildBTWithPreOrder(T[] dataArray, AtomicInteger i){
+		if(i.get() > dataArray.length || dataArray[i.get()] == null) {
 			return null;
 		}
-		BTNode<String> node = new BTNode<String>(dataArray[i.get()]);
+		BTNode<T> node = new BTNode<T>(dataArray[i.get()]);
 		i.incrementAndGet();
 		node.left = buildBTWithPreOrder(dataArray, i);
 		i.incrementAndGet();
@@ -52,26 +47,56 @@ public class BTNode<T extends Comparable<T>> {
 		return node;
 	}
 	
-	//Since binary tree is is unbalanced and not sorted, we have to lookup on all the nodes.
-	//The search is n here on worst case not log(n)
-	//The search is log(n) in BST
-	public static BTNode<String> findBTNodeForData(BTNode<String> root, String data){
+	
+	//Since binary tree is unbalanced and not sorted, we have to lookup on all the nodes.
+	//The search is worst case is n here.
+	public static <T extends Comparable<T>>  BTNode<T> findBTNodeForData(BTNode<T> root, T data){
         if(root == null || root.data.equals(data)) {
         	return root;
         }
-        BTNode<String> foundNode =  findBTNodeForData(root.left, data);
+        BTNode<T> foundNode =  findBTNodeForData(root.left, data);
         if(foundNode == null) {
         	foundNode = findBTNodeForData(root.right, data);
         }
 		return foundNode;
 	}
 	
-	public static void buildBTWithInOrder(Object[] dataArray){
-//		BTNode<Object> rt = new BTNode<Object>(dataArray[0]);
-		
+	
+	//Since binary Search tree is sorted, we search node in log n time.
+	public static <T extends Comparable<T>>  BTNode<T> findBSTNodeForData(BTNode<T> root, T data){
+        if(root == null || root.data.equals(data)) {
+        	return root;
+        }
+        BTNode<T> foundNode =  findBTNodeForData(root.left, data);
+        if(foundNode == null) {
+        	foundNode = findBTNodeForData(root.right, data);
+        }
+		return foundNode;
 	}
 	
 
+	static class BTNodeComparator<T extends Comparable<T>> implements Comparator<BTNode<T>>{
+		public static  BTNodeComparator compator = new BTNodeComparator();
+		
+		@Override
+		public int compare(BTNode<T> o1, BTNode<T> o2) {
+			return o1.data.compareTo(o2.data);
+		}
+		
+	}
+	
+	
+	
+	/*******************************************************/
+	/*******************************************************/
+	/*** Check the possibility of building the tree with ***/
+	/***         other traversal technique               ***/
+	/*******************************************************/
+	/*******************************************************/
+	
+	public static void buildBTWithInOrder(Object[] dataArray){
+//		BTNode<Object> rt = new BTNode<Object>(dataArray[0]);
+	}
 	
 	private static <T extends Comparable<T>> BTNode<T> buildBTWithInOrder(T data, BTNode<T> parent, BTNode<T> child, boolean isLeft){
 		if(child == null) {
@@ -83,7 +108,6 @@ public class BTNode<T extends Comparable<T>> {
 			}
 			return node;
 		}
-		
 		return null;
 //		if(parent.data < data) {
 //			buildBTWithInOrder(data, parent, parent.left, true);
@@ -93,12 +117,4 @@ public class BTNode<T extends Comparable<T>> {
 //		}
 	}
 	
-	static class BTNodeComparator<T extends Comparable<T>> implements Comparator<BTNode<T>>{
-
-		@Override
-		public int compare(BTNode<T> o1, BTNode<T> o2) {
-			return o1.data.compareTo(o2.data);
-		}
-		
-	}
 }
