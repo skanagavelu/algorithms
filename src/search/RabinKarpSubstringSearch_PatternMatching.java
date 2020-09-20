e3c5fb1fd7826d81fdfdeb0ec083471cfe82b51c
Kanagavelu Sugumar <skanagavelu@users.noreply.github.com>
2020-09-20 14:57:40 +0530

    Update RabinKarpSubstringSearch_PatternMatching.java

--- src/search/RabinKarpSubstringSearch_PatternMatching.java
+++ src/search/RabinKarpSubstringSearch_PatternMatching.java
@@ -37,28 +37,55 @@ public class RabinKarpSubstringSearch_PatternMatching {
          </PRE>
 	 * @param args
 	 */
-	
-	public static void main(String[] args) {
-		String input = "ABCDABCEBCRBBA";
-		String substring = "BCR";
-		System.out.println(isPatternMatches(input, substring));
-	}
+        char[] mainString = "aabc".toCharArray();
+        char[] subString = "abc".toCharArray();
+        int subStringHash = buildHash(subString, 0, subString.length);
+        //hash for first three char
+        int mainStringHash = buildHash(mainString, 0, subString.length); //exclusive subString.length
 
-	private static boolean isPatternMatches(String input, String substring) {
-		int hashValueOfSubString = findTheInitialHash(substring, substring.length());
-		int hashValueOfInputString = findTheInitialHash(substring, substring.length());
-		char [] inputCharArray = input.toCharArray();
-		
-		for(int i = substring.length(); i < input.length(); i ++){
-			hashValueOfInputString = findTheRollingHash(substring, i, i-substring.length(), hashValueOfInputString);
-		}
-		return false;
-	}
+        //This is needed for initial match found, useful when mainString=abc... and subString=abc
+        if (mainStringHash == subStringHash &&
+            Arrays.equals(Arrays.copyOfRange(mainString, 0, subString.length), subString)) {
+            System.out.println(true);
+            return;
+        }
 
-	private static int findTheInitialHash(String substring, int length) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
-	
+        //we have to remove 0th char, and add with new char in the hash, since it is not matched above
+        for (int i = 0; i < mainString.length-subString.length; i++) {
+
+            /*  building rolling hash:
+             * -------------------------
+             *  new_hash = old_hash - val(old_char)
+             *  new_hash = new_hash / prime
+             *  new_hash = new_hash + Math.pow(prime, subStringLength) + val(new_char)
+             */
+            mainStringHash = mainStringHash - mainString[i];
+            mainStringHash = mainStringHash / hash;
+
+
+            // mainString[i+subString.length] : "babc" initial char "b" is ignored, and we need to include till "c",
+            // hence subString.length
+            // Math.pow(hash, subString.length-1):first char pow is 0, so for three char length the pow goes to max 2
+            mainStringHash = mainStringHash + (int)Math.pow(hash, subString.length-1) * mainString[i+subString.length];
+
+            // i+1;  because it is computed for i+1th char after removing ith char, ith char is already not matched
+            // before entering this loop, still we should start with 0, to remove 0th index from initial hash.
+            if (mainStringHash == subStringHash &&
+                Arrays.equals(Arrays.copyOfRange(mainString, i+1, i+1  + subString.length), subString)) {
+                System.out.println(true);
+                return;
+            }
+        }
+        System.out.println(false);
+    }
+
+    public static int buildHash(char[] subString, int offset, int length) {
+
+        int result = 0; // Ensure first char is simple ASCII value, so that we can remove
+        int power = 0;  // Is need so that we can divide and move the char back
+        for (int i = offset; i < length; i++) {
+            result = result + ((int)Math.pow(hash, power++) * subString[i]); //power is 0, 1, 2  == for 3 char length.
+        }
+        return result;
+    }
 }
