f574195e78100ced8e73b922a91a713da25f99cf
Kanagavelu Sugumar <skanagavelu@users.noreply.github.com>
2020-10-16 15:45:29 +0530

    Update NQueens.java

--- src/backtracking/NQueens.java
+++ src/backtracking/NQueens.java
@@ -3,40 +3,36 @@ import java.util.Arrays;
 // https://www.youtube.com/watch?v=xouin83ebxE
 public class NQueens {
 
-    //Queen position
-    class Position {
-        int row, col;
-        Position(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-    }
+    public int[] solveNQueenOneSolution(int n) {
 
-    public Position[] solveNQueenOneSolution(int n) {
-        
-        //Queen column position on each row
-        Position[] positions = new Position[n];
+        //Queen column position on every index row
+        int[] positions = new int[n];
         boolean hasSolution = solveNQueenOneSolutionUtil(0, positions);
         if (hasSolution) {
             return positions;
         } else {
-            return new Position[0];
+            return new int[0];
         }
     }
 
-    private boolean solveNQueenOneSolutionUtil(int row, Position[] positions) {
+    //Starts from first row, and go till end of the row
+    private boolean solveNQueenOneSolutionUtil(int row, int[] positions) {
+
         if (positions.length == row) {
-            // All rows; queens are placed
+            // All rows reached; means all queens are placed
             return true;
         }
 
-        //Check for next valid column in this row
+        //Check for next valid column in current row
         for (int col = 0; col < positions.length; col++) {
 
             if (isSafe(row, col, positions)) {
-                positions[row] = new Position(row, col);
+                positions[row] = col;
 
-                // Backtracking Pattern: returns success or failure to the caller
+                /* Backtracking Pattern:
+                 * 1. Call with incremental value || Find the column for next row
+                 * 2. returns (ends stack) success or failure to the caller
+                 */
                 if (solveNQueenOneSolutionUtil(row + 1, positions)) {
                     return true;
                 }
@@ -45,13 +41,18 @@ public class NQueens {
         return false;
     }
 
-    boolean isSafe(int row, int col, Position[] positions) {
+    private boolean isSafe(int row, int col, int[] positions) {
 
         //check if this row and col is not under attack from any previous queen position
-        for (int pos = 0; pos < row; pos++) {
-            if (positions[pos].col == col ||
-                positions[pos].row - positions[pos].col == row - col ||
-                positions[pos].row + positions[pos].col == row + col) {
+        for (int row0 = 0; row0 < row; row0++) {
+
+            if (positions[row0] == col ||   // Same column?
+
+                //Two diagonal check
+                // 1. (previous queen row + col) == (current queen row + col)?
+                // 2. (previous queen row - col) == (current queen row - col)?
+                row0 - positions[row0] == row - col ||
+                row0 + positions[row0] == row + col) {
                 return false;
             }
         }
@@ -60,7 +61,7 @@ public class NQueens {
 
     public static void main(String args[]) {
         NQueens s = new NQueens();
-        Position[] positions = s.solveNQueenOneSolution(4);
-        Arrays.stream(positions).forEach(position -> System.out.println(position.row + " " + position.col));
+        int[] positions = s.solveNQueenOneSolution(4);
+        Arrays.stream(positions).forEach(column -> System.out.println(column));
     }
 }
