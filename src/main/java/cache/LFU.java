dc13668f3dfb4b05324ffde9663ee95902c0815e
skanagavelu <skanagavelu@gmail.com>
2020-10-24 08:56:51 +0530

    LRU Support

---
+++ src/main/java/cache/LFU.java
@@ -0,0 +1,9 @@
+package cache;
+
+/**
+ * http://javarticles.com/2012/06/lfu-cache.html
+ * http://stackoverflow.com/questions/17759560/what-is-the-difference-between-lru-and-lfu
+ */
+public class LFU {
+
+}
--- src/main/java/cache/LRU.java
+++ src/main/java/cache/LRU.java
@@ -1,5 +1,35 @@
 package cache;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LRU {
 
+    private LinkedHashMap<Integer,Integer> cache;
+    public LRU(int capacity) {
+        this.cache = new LinkedLRUMap(capacity);
+    }
+
+    public int get(int key) {
+        Integer val = cache.get(key);
+        return val == null ? -1 : val;
+    }
+
+    public void set(int key, int value) {
+        cache.put(key, value);
+    }
+
+    class LinkedLRUMap extends LinkedHashMap<Integer, Integer> {
+
+        int capacity;
+        LinkedLRUMap(int capacity) {
+            super(capacity, 0.75f, true);
+            this.capacity = capacity;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> entry) {
+            return size() > capacity;
+        }
+    }
 }
