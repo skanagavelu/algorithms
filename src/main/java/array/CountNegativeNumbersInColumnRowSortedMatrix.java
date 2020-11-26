5a0c02ab3325919c6674b9d723115d4f2a6d439b
skanagavelu <skanagavelu@gmail.com>
2020-11-26 18:48:10 +0530

    .

--- src/main/java/array/CountNegativeNumbersInColumnRowSortedMatrix.java
+++ src/main/java/array/CountNegativeNumbersInColumnRowSortedMatrix.java
@@ -1,53 +1,43 @@
 package array;
 
 /**
- * 
  * http://www.geeksforgeeks.org/count-negative-numbers-in-a-column-wise-row-wise-sorted-matrix/
- * @author KanagaveluSugumar
  *
+ * @author sugumar
  */
 public class CountNegativeNumbersInColumnRowSortedMatrix {
-	
-	public static void main(String[] args) {
-		findSqureSubMatrix(inputArray);
-	}
 
-	private static int[][] inputArray = {
-			                       {0,  1,  1,  0,  1},
-			                       {1,  1,  0,  1,  0},
-			                       {0,  1,  1,  1,  0},
-			                       {1,  1,  1,  1,  0},
-			                       {1,  1,  1,  1,  1},
-			                       {0,  0,  0,  0,  0},
-	                             };
-	
-	private static void findSqureSubMatrix(int[][] inputArray) {
-		
-		int[][] inMemory = new int[inputArray.length][inputArray[0].length];
-		int maxElementValue = 0;
-		int maxElementRow = 0;
-		int maxElementCol = 0;
+    public static void main(String[] args) {
 
-		
-//		inMemory = Arrays.copyOf(original, newLength)
-		
-		for(int i = 1; i < inputArray.length; i++) {
-			for(int j = 1; j < inputArray[0].length; j++) {
-				if(inputArray[i][j] == 1) {
-					inputArray[i][j] = Math.min(Math.min(inputArray[i-1][j], inputArray[i][j-1]), inputArray[i-1][j-1]) + 1;
-				} else {
-					inputArray[i][j] = 0;
-				}
-				maxElementValue = Math.max(maxElementValue, inputArray[i][j]);
-				if(maxElementValue == inputArray[i][j]) {
-					maxElementRow = i;
-					maxElementCol = j;
-				}
-			} 
-		}
-		System.out.println("maxElementValue: " + maxElementValue);
-		
-	}
+        countNegativeNumbers(sortedArray);
+    }
+
+    private static int[][] sortedArray = {
+            { -3, -2, -1, 1 },
+            { -2,  2,  3, 4 },
+            {  4,  5,  7, 8 }
+    };
+
+    private static void countNegativeNumbers(int[][] sortedArray) {
+
+        int negativeCount = 0;
+
+        /*
+         *  We start from the top right corner and find the position of the last negative number in the first row.
+         *  Using this information, we find the position of the last negative number in the second row.
+         *  We keep repeating this process until we either run out of negative numbers or we get to the last row.
+         */
+        for (int col = sortedArray[0].length - 1, row = 0; col >= 0 && row < sortedArray.length; ) {
+
+            if (sortedArray[row][col] < 0) {
+                negativeCount += col + 1;
+                row ++;
+            } else {
+                col--;
+            }
+        }
+        System.out.println("negativeCount: " + negativeCount);
+    }
 }
 
 
